148fb0ba2f248fdad6f7427566f6f97310179fb7
KaanHD79 <81617346+KaanHD79@users.noreply.github.com>
2023-03-24 14:08:32 +0100

    first beta
    almost complete rewrite of the system changed like everything

--- Ampelsystem.py
+++ Ampelsystem.py
@@ -1,62 +1,60 @@
 import RPi.GPIO as GPIO
 import time
-
 GPIO.setwarnings(False)
-
 GPIO.setmode(GPIO.BCM)
-
-# Questions
-portgreen1 = 25
-portyellow1 = 25
-portred1 = 25
-portgreen2 = 25
-portred2 = 25
-portbutton = 10
-# ampel grün wie lange gelb
-ampel1ytgt = 3
-# Ampel rot wie lange gelb
-ampel1ytrt = 3
-# Fuß wie lange grün
-ampel2gtrt = 10
-# Puffer
+# Pin mappings
+pins = {
+    "ampel1_green": 25,
+    "ampel1_yellow": 24,
+    "ampel1_red": 23,
+    "ampel2_green": 22,
+    "ampel2_red": 21,
+    "button": 10,
+}
+# Timing constants
+ampel1_yellow_to_green_time = 3
+ampel1_yellow_to_red_time = 3
+ampel2_green_time = 10
 puffer = 1
-
-
-#Ports bestimmen
-GPIO.setup(portgreen1, GPIO.OUT)
-GPIO.setup(portyellow1, GPIO.OUT)
-GPIO.setup(portred1, GPIO.OUT)
-GPIO.setup(portgreen2, GPIO.OUT)
-GPIO.setup(portred2, GPIO.OUT)
-GPIO.setup(portbutton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-def ampeltored():
-    GPIO.output(portgreen1, GPIO.LOW)
-    GPIO.output(portyellow1, GPIO.HIGH)
-    time.sleep(ampel1ytrt)
-    GPIO.output(portyellow1, GPIO.LOW)
-    GPIO.output(portred1, GPIO.HIGH)
-    time.sleep(puffer)
-def ampeltogreen():
-    GPIO.output(portred1, GPIO.LOW)
-    GPIO.output(portyellow1, GPIO.HIGH)
-    time.sleep(ampel1ytgt)
-    GPIO.output(portyellow1, GPIO.LOW)
-    GPIO.output(portgreen1, GPIO.HIGH)
-    time.sleep(puffer)
-def ampeltogreen2():
-    GPIO.output(portred2, GPIO.LOW)
-    GPIO.output(portgreen2, GPIO.HIGH)
-    time.sleep(ampel2gtrt)
-    GPIO.output(portgreen2, GPIO.LOW)
-    GPIO.output(portred2, GPIO.HIGH)
-    time.sleep(puffer)
-
-
-GPIO.output(portgreen1, GPIO.HIGH)
-GPIO.output(portred2, GPIO.HIGH)
-
-if GPIO.input(portbutton) == GPIO.HIGH:
-    ampeltored()
-    ampeltogreen2()
-    ampeltogreen()
+# Setup GPIO pins
+for pin in pins.values():
+    if "button" in pins and pins["button"] == pin:
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    else:
+        GPIO.setup(pin, GPIO.OUT)
+# Function to control the traffic light
+def control_traffic_light(ampel1_yellow_to_green_time, ampel1_yellow_to_red_time, ampel2_green_time, puffer):
+    # Set initial states
+    GPIO.output(pins["ampel1_green"], GPIO.HIGH)
+    GPIO.output(pins["ampel2_red"], GPIO.HIGH)
+    while True:
+        try:
+            # Check button state
+            if GPIO.input(pins["button"]) == GPIO.HIGH:
+                # Set traffic light to red for first intersection
+                GPIO.output(pins["ampel1_green"], GPIO.LOW)
+                GPIO.output(pins["ampel1_yellow"], GPIO.HIGH)
+                time.sleep(ampel1_yellow_to_red_time)
+                GPIO.output(pins["ampel1_yellow"], GPIO.LOW)
+                GPIO.output(pins["ampel1_red"], GPIO.HIGH)
+                time.sleep(puffer)
+                # Set traffic light to green for second intersection
+                GPIO.output(pins["ampel2_red"], GPIO.LOW)
+                GPIO.output(pins["ampel2_green"], GPIO.HIGH)
+                time.sleep(ampel2_green_time)
+                GPIO.output(pins["ampel2_green"], GPIO.LOW)
+                GPIO.output(pins["ampel2_red"], GPIO.HIGH)
+                time.sleep(puffer)
+                # Set traffic light to green for first intersection
+                GPIO.output(pins["ampel1_yellow"], GPIO.HIGH)
+                time.sleep(ampel1_yellow_to_green_time)
+                GPIO.output(pins["ampel1_red"], GPIO.LOW)
+                GPIO.output(pins["ampel1_yellow"], GPIO.LOW)
+                GPIO.output(pins["ampel1_green"], GPIO.HIGH)
+        except Exception as e:
+            print(e)
+            GPIO.cleanup()
+            break
+# Start controlling the traffic light
+# Main
+control_traffic_light(ampel1_yellow_to_green_time, ampel1_yellow_to_red_time, ampel2_green_time, puffer)
